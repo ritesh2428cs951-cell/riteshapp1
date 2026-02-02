5e2e0d92e15c742ed1ebafe06d1d6eff7a66723d
ritesh gautam <ritesh.242cs951@kiet.edu>
2026-02-02 16:27:59 +0530

    intial commit

---
+++ demo.js
@@ -0,0 +1,1 @@
+npx create vite@latest
---
+++ myapp1/src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+ export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    
+    <div>
+
+   
+    <h1>hello ritesh </h1>
+    <h2> how this collage </h2>
+
+      
+    </div>
+
+  )
+}
+
+//  export default App
